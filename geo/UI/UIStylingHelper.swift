27419bca891f0c45ff6969c5e2cc5ab05bbf37db
panandafog <panandafog@gmail.com>
2023-04-06 15:35:00 +0300

    changed buttons shadow opacity

--- geo/UI/UIStylingHelper.swift
+++ geo/UI/UIStylingHelper.swift
@@ -29,7 +29,7 @@ enum UIStylingHelper {
         button.backgroundColor = .secondarySystemBackground
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        button.layer.shadowOpacity = 0.2
+        button.layer.shadowOpacity = 0.15
         button.layer.shadowRadius = 5.0
         button.layer.cornerRadius = 5.0
     }
